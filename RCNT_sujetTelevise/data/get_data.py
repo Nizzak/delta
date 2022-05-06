2a66a3c043199087351d2a117d210f460d0d8302
Nizzak <73716402+Nizzak@users.noreply.github.com>
2022-05-06 15:19:40 +0200

    Update get_data.py
    Version finale commentée.

--- RCNT_sujetTelevise/data/get_data.py
+++ RCNT_sujetTelevise/data/get_data.py
@@ -1,24 +1,33 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # #
-# Nos deux bases de données proviennent de l INA  #
-# # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Nos deux bases de données proviennent de l'INA, elles sont disponibles aux URLs suivantes #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-url_nb = "https://static.data.gouv.fr/resources/classement-thematique-des-sujets-de-journaux-televises-janvier-2005-septembre-2020/20201202-114045/ina-barometre-jt-tv-donnees-mensuelles-2005-2020-nombre-de-sujets.csv"
+url_count = "https://static.data.gouv.fr/resources/classement-thematique-des-sujets-de-journaux-televises-janvier-2005-septembre-2020/20201202-114045/ina-barometre-jt-tv-donnees-mensuelles-2005-2020-nombre-de-sujets.csv"
 url_time =  "https://static.data.gouv.fr/resources/classement-thematique-des-sujets-de-journaux-televises-janvier-2005-septembre-2020/20201202-114231/ina-barometre-jt-tv-donnees-mensuelles-2005-2020-durees.csv"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Nous pouvons directement les télécharger sous format .csv   #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+url_content_count = requests.get(url_count).content
+csv_file_count = open('ina-barometre-jt-tv-donnees-mensuelles-2005-2020-nombre-de-sujets.csv', 'wb')
+csv_file_count.write(url_content_count)
+csv_file_count.close()
+
+url_content_time = requests.get(url_time).content
+csv_file_time = open('ina-barometre-jt-tv-donnees-mensuelles-2005-2020-durees.csv', 'wb')
+csv_file_time.write(url_content_time)
+csv_file_time.close()
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # # #
-# Probleme dans le format du csv. On retire les ';' en trop...     #
-# # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # # #
-input_file =
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Probleme dans le format du csv. dans le fichier 'ina-barometre-jt-tv-donnees-mensuelles-2005-2020-nombre-de-sujets.csv' on retire les ';' en trop à la fin des lignes #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+input_file = 'ina-barometre-jt-tv-donnees-mensuelles-2005-2020-nombre-de-sujets.csv'
 
 # Open the file as read
-f = open(input_file, "r+")
+f = open('data_count.csv', "r+")
 # Create an array to hold write data
 new_file = []
 # Loop the file line by line
@@ -29,4 +38,6 @@ for line in f:
 with open(input_file, "w+") as f:
     for i in new_file:
         f.write(i + "\n")
-
+# # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # # # # # # # # # # # # # # # # #
+# Toutes les données seront utilisées dans les analyses, il n'y a donc pas besoin d'en enlever #
+# # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # # # # # # # # # # # # # # # # #
